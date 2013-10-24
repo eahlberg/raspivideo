d0efdc4ce2fca7947d0381b48123c1e2dee5cd7a
Eric Ahlberg <eahlberg@gmail.com>
2013-10-24 21:11:36 +0200

    Movie playback from JS GUI working

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-#!flask/bin/python
+#!/usr/bin/env python
 from flask import Flask, jsonify, make_response, abort, render_template
 import subprocess
 import json
@@ -6,7 +6,8 @@ import os
 
 app = Flask(__name__)
 
-PATH = '/Volumes/Data/filmer'
+#PATH = '/Volumes/Data/filmer'
+PATH = '/home/pi/timecapsule/filmer'
 FILE_EXT1 = '*.avi'
 output = subprocess.check_output(['find', PATH, '-name',
      FILE_EXT1])[0:-1].split(b'\n')
@@ -20,7 +21,7 @@ for i in xrange(0, len(output)):
     }
     movies.append(movie)
 
-
+print movies
 @app.route('/raspivideo')
 def index():
     return render_template('main.html')
@@ -40,11 +41,14 @@ def get_movie(movie_id):
 def play_movie(movie_id):
     movie = filter(lambda t : t['id'] == movie_id, movies)
     movie_path = movie[0]['title']
-    vlc_path = '/Applications/VLC.app/Contents/MacOS/VLC'
-    cmd = vlc_path + ' "' + movie_path + '"'
-    print cmd
-    subprocess.call(cmd, shell=True)
-    #os.system('echo ' + movie[0])
+    #vlc_path = '/Applications/VLC.app/Contents/MacOS/VLC'
+    omx_path = 'omxplayer '
+    #cmd = vlc_path + ' "' + movie_path + '"'
+    
+    cmd_pi = omx_path + '"' + movie_path + '"'
+    os.system(cmd_pi)
+    #subprocess.call(cmd, shell=True)
+    #subprocess.call(cmd_pi, shell=True)
     return jsonify( { 'starting movie': movie } )
 
 
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,14 +1,11 @@
 $(function() {
 
-    // baseUri needs to be the same as in app.py's app.run(URI)
-    // to avoid ajax cross domain issues
-    var baseUri = "http://0.0.0.0:5000/raspivideo/movies";
+    var baseUri = "http://10.0.1.5:5000/raspivideo/movies";
     var playUri = "play";
 
     $("#list_movies").click(function() {
         $("tbody").empty();
-        $("table").append("<thead><tr><th>id</th><th>title</th></tr></thead>");
-        alert("btn clicked");
+        $("table").append("<thead><tr><th>id</th><th>title</th></tr></thead>");;
         var deferred = $.getJSON(baseUri);
 
         deferred.done(function(obj) {
@@ -17,7 +14,8 @@ $(function() {
                 $("tbody").append("<tr><td>" + movie.id
                     + "</td><td>" + movie.title + "</td>"
                     + '<td><button id="play' + movie.id + '">play</button></td></tr>');
-                $("#play" + movie.id).click(function() { 
+                $("#play" + movie.id).click(function() {
+		    alert("play clicked");
                     var def = $.getJSON(baseUri + "/" + playUri + "/" + movie.id);
                     def.done(function(obj) {
                         console.log("sending play request");
