f7d1fa3d5e56cb1f43cc3768285a408088be91d3
Eric Ahlberg <eahlberg@gmail.com>
2014-10-13 16:17:19 +0200

    Remove static path to media - Add settings.cfg to gitignore

--- src/app/config.py
+++ src/app/config.py
@@ -10,15 +10,14 @@ class Config:
     def __init__(self):
         self.path = os.getcwd() + '/app'
         self.cfg_path = self.path + '/settings.cfg'
-        self.movie_path = '/mnt/time_capsule/filmer'
         self.last_played = 'none'
         self.running_time = 'none'
         if not os.path.isfile(self.cfg_path):
+            print '[APP] no config found, creating settings.cfg'
             parser = ConfigParser.SafeConfigParser()
             parser.add_section('info')
             parser.set('info', 'path', self.path)
             parser.set('info', 'config path', self.cfg_path)
-            parser.set('info', 'movie path', self.movie_path)
             parser.set('info', 'last played', self.last_played)
             parser.set('info', 'running time', self.running_time)
             with open(self.cfg_path, 'wb') as file:
@@ -30,7 +29,12 @@ class Config:
         """
         parser = ConfigParser.SafeConfigParser()
         parser.read(self.cfg_path)
-        return parser.get('info', item)
+        try:
+            value = parser.get('info', item)
+            return value
+        except ConfigParser.NoOptionError as e:
+            print e
+            return None
 
     def add(self, item, value):
         """
